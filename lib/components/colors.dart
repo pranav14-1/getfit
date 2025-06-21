b5326f44420a92fc7d18fc4c501664bb0f33ae62
Pranav <thakurpranav14.1@gmail.com>
2025-06-22 02:11:11 +0530

    Initial commit

---
+++ lib/components/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color font1 = Colors.black;
+  static Color font2 = Colors.white;
+  static Color link = Colors.blue;
+  static Color background = Colors.white;
+  static Color buttons = Colors.black;
+}
