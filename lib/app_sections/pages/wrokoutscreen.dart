3c6097fa98efc9e187642485f279ea2d9cb328b0
Pranav <thakurpranav14.1@gmail.com>
2025-06-26 22:04:35 +0530

    minor changes

--- lib/app_sections/pages/wrokoutscreen.dart
+++ lib/app_sections/pages/wrokoutscreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:getfit/components/colors.dart';
-import 'package:fl_chart/fl_chart.dart';
-import 'package:intl/intl.dart';
+// import 'package:fl_chart/fl_chart.dart';
+// import 'package:intl/intl.dart';
 
 class Workoutscreen extends StatefulWidget {
   const Workoutscreen({super.key});
--- lib/components/bar_chart.dart
+++ lib/components/bar_chart.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fl_chart/fl_chart.dart';
+// import 'package:fl_chart/fl_chart.dart';
 import 'package:intl/intl.dart';
 
 class BarChart extends StatelessWidget {
